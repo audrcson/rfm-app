c016d23dc51fc5438dd9b499df9c3ee01a143ba4
Jenni Febiyola Sari <halo.jenni01@gmail.com>
2025-07-11 16:18:50 +0700

    updatedd

--- pages/rfm/[id].jsx
+++ pages/rfm/[id].jsx
@@ -32,40 +32,37 @@ export default function RFMDetail({ rfm, error }) {
   if (!rfm) return <p>Data not found</p>;
 
   return (
-  <div className="min-h-screen bg-gray-100 flex items-center justify-center p-6">
-    <div className="bg-white rounded-xl shadow-md w-full max-w-xl p-6 border border-gray-300">
-      
-      {/* Bagian Atas: Info Mesin */}
-      <div className="mb-4">
-        <p className="text-sm text-gray-600">
-          <strong>{rfm.kode_mesin} - {rfm.nama_mesin}</strong> 
-        </p>
-      </div>
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center p-6">
+      <div className="bg-white rounded-xl shadow-md w-full max-w-xl p-6 border border-gray-300">
+        
+        {/* Bagian Atas: Info Mesin */}
+        <div className="mb-4">
+          <p className="text-sm text-gray-600">
+            <strong>{rfm.kode_mesin} - {rfm.nama_mesin}</strong> 
+          </p>
+        </div>
 
-      {/* Judul */}
-      <h1 className="text-center text-xl font-bold text-green-600 mb-4">
-        RFM (Request For Maintenance)
-      </h1>
+        {/* Judul */}
+        <h1 className="text-center text-xl font-bold text-green-600 mb-4">
+          RFM (Request For Maintenance)
+        </h1>
 
-      {/* List Informasi */}
-      <div className="divide-y divide-gray-300 text-gray-700 text-sm">
-        <p className="py-2">
-          <strong>No RFM:</strong> {rfm.no_rfm}
-        </p>
-        <p className="py-2">
-          <strong>Tgl Mesin Rusak / Bermasalah:</strong> {rfm.tanggal_kerusakan}
-        </p>
-        <p className="py-2">
-          <strong>Deskripsi:</strong> {rfm.deskripsi}
-        </p>
-        <p className="py-2">
-          <strong>Aksi Penanganan:</strong> {rfm.action_handling}
-        </p>
-        <p className="py-2">
-          <strong>No KJB:</strong> {rfm.kbj}
-        </p>
+        {/* List Informasi */}
+        <div className="divide-y divide-gray-300 text-gray-700 text-sm">
+          <p className="py-2">
+            <strong>No. RFM:</strong> {rfm.no_rfm}
+          </p>
+          <p className="py-2">
+            <strong>Tgl Mesin Rusak / Bermasalah:</strong> {rfm.tanggal_kerusakan}
+          </p>
+          <p className="py-2">
+            <strong>Deskripsi:</strong> {rfm.deskripsi}
+          </p>
+          <p className="py-2">
+            <strong>No. KJB:</strong> {rfm.kbj}
+          </p>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
 }
