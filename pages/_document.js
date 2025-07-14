150ab0a549caedc78345abb66f581706390b4006
Jenni Febiyola Sari <halo.jenni01@gmail.com>
2025-07-14 10:50:13 +0700

    updatedd

--- pages/_document.js
+++ pages/_document.js
@@ -1,18 +1,14 @@
+// pages/_document.js
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        {/* Tambahkan aturan print di sini */}
-        <style>
-          {`
-            @page {
-              size: A4 portrait;
-              margin: 1cm;
-            }
-          `}
-        </style>
+        <style>{`
+          @page { size: A4 portrait; margin: 1cm; }
+          .page-break { page-break-after: always; }
+        `}</style>
       </Head>
       <body className="antialiased">
         <Main />
